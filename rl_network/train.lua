1ffc9dabbfc4b3d4f5ee677ff177e223452c9c7d
gigaflw <a_gigantic_flower@yeah.net>
2017-12-17 17:34:04 +0800

    move require selfplay after set device

--- rl_network/train.lua
+++ rl_network/train.lua
@@ -7,10 +7,7 @@ package.path = package.path .. ';../?.lua'
 
 require("_torch_class_patch")
 
-local self_play = require("rl_network.self_play")
-local rl_utils = require("rl_network.rl_utils")
 local pl = require 'pl.import_into'()
-
 local opt = pl.lapp[[
     --model_name           (default "resnet_16")    Code name for models. If this is not empty then --input will be omitted.
     --step_threshold       (default 1)              If the step of a game is less than the threshold, it is a bad sample.
@@ -25,8 +22,6 @@ local opt = pl.lapp[[
     --device             (default 3)        which core to use on a multicore GPU environment
 ]]
 
-local opt = rl_utils.rl_init(opt)
-
 local util = require 'resnet.util'
 opt.use_gpu = opt.use_gpu and util.have_gpu() -- only use gpu when there is one
 
@@ -36,4 +31,8 @@ if opt.use_gpu then
     print('use gpu device '..opt.device)
 end
 
+local rl_utils = require("rl_network.rl_utils")
+local self_play = require("rl_network.self_play")
+
+local opt = rl_utils.rl_init(opt)
 self_play.train(opt)
